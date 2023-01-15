bfcf2cb5f66d08b868593b07670eadcb930d015f
jrivilla <jrivillarepository@gmail.com>
2023-01-15 12:59:32 +0100

    feat: signup screen and authorization with firebase

--- netflix-build/src/App.js
+++ netflix-build/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './HomeScreen';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-     <HomeScreen/>
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/test" element={<h1>Test</h1>} />
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
--- netflix-build/src/firebase.js
+++ netflix-build/src/firebase.js
@@ -1,8 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 const firebaseConfig = {
-    apiKey: "AIzaSyDNzkCGHXVw0I18k7QBsJ-NftPfbNq62Dk",
-    authDomain: "netflix-clone-cd0db.firebaseapp.com",
-    projectId: "netflix-clone-cd0db",
-    storageBucket: "netflix-clone-cd0db.appspot.com",
-    messagingSenderId: "221878904861",
-    appId: "1:221878904861:web:4241251aa01c28fe563ef4"
-  };
+  apiKey: "AIzaSyDNzkCGHXVw0I18k7QBsJ-NftPfbNq62Dk",
+  authDomain: "netflix-clone-cd0db.firebaseapp.com",
+  projectId: "netflix-clone-cd0db",
+  storageBucket: "netflix-clone-cd0db.appspot.com",
+  messagingSenderId: "221878904861",
+  appId: "1:221878904861:web:4241251aa01c28fe563ef4",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+function registerWithEmailAndPassword(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+function logInWithEmailAndPassword(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export { auth, registerWithEmailAndPassword, logInWithEmailAndPassword };
+export default db;
